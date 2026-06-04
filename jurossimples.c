bee6a0d4aa2068d037095484ad775b32fabc41f3
Luís Guilherme <luis.bl.guilherme@gmail.com>
2026-06-04 19:12:09 -0300

    jurossimples.c

--- jurossimples.c
+++ jurossimples.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-#include "function_jurossimples.c"
 
-int main() {
-    double capital, taxa, tempo;
+float jurossimples(float capital, float taxa, int tempo);
 
-    printf("Digite o capital inicial: ");
-    scanf("%lf", &capital);
+int main()
 
-    printf("Digite a taxa de juros (em decimal, ex: 0.10 para 10%%): ");
-    scanf("%lf", &taxa);
+{
+    float capital, taxa;
+    int tempo;
 
-    printf("Digite o tempo: ");
-    scanf("%lf", &tempo);
+    printf("Capital: ");
+    scanf("%f", &capital);
 
-    double juros = calcularJuros(capital, taxa, tempo);
-    double montante = capital + juros;
+    printf("Taxa: ");
+    scanf("%f", &taxa);
 
-    printf("Juros (J): %.2f\n", juros);
-    printf("Montante (M): %.2f\n", montante);
+    printf("Tempo: ");
+    scanf("%d", &tempo);
 
-    printf("Criado por Luís Guilherme");
+    printf("Juros = %.2f\n", jurossimples(capital, taxa, tempo));
+
+    printf("\nFeito por Luís Guilherme ( https://github.com/luisblguilherme-11 ) ");
+
     return 0;
 }
